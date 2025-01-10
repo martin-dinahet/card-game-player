138b530d49fe5b724e975d55eab9b7f4b3189f6a
Martin <martin.dinahet@proton.me>
2025-01-10 01:09:36 +0100

    added react router dependency and setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Index } from "@/pages/Index";
+import { BrowserRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
 
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Index />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />}></Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
